a71f7e3c41ac3260ed254fd56b85a9baf8f09b71
Ashu2607 <ashutosh.snh@iitg.ac.in>
2025-04-15 21:00:03 +0530

    Update app.py

--- My_Website/app.py
+++ My_Website/app.py
@@ -33,7 +33,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Profile Section
-image_path = "circular_profile.png"  
+image_path = "./circular_profile.png"  
 
 if os.path.exists(image_path):
     circular_image = make_circular(image_path)
